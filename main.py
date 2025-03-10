7052fbc76fe6de90ed5a362209fa229ff475d534
Obi-Wan1977 <cjhoewing@gmail.com>
2025-03-10 08:54:07 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-# render_template("name_of_template.html")
+render_template("name_of_template.html")
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -11,16 +11,6 @@ def form():
 def hello():
    message = "Hello, Flask!"
    return message
-
-@app.route('/thanks')
-def thanks():
-      person = "Bob"
-      shoes = "tapshoes"
-      action = "dancing"
-      being = "person"
-      closing = "Sincerely,"
-      name2 = "Caleb"
-      return render_template("tynote.html", name = person, gift = shoes, verb = action, noun = being, closing_word = closing, author = name2)
 
 
 
