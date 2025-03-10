4d3cab585dbefa2fb7fd8f9db2655c0f71748e53
Obi-Wan1977 <cjhoewing@gmail.com>
2025-03-10 08:55:58 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-render_template("name_of_template.html")
+from flask import Flask, render_template, request
+# render_template("name_of_template.html")
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
@@ -12,9 +12,40 @@ def hello():
    message = "Hello, Flask!"
    return message
 
+@app.route('/thanks')
+def thanks():
+      person = "Bob"
+      shoes = "tapshoes"
+      action = "dancing"
+      being = "person"
+      closing = "Sincerely,"
+      name2 = "Caleb"
+      return render_template("tynote.html", name = person, gift = shoes, verb = action, noun = being, closing_word = closing, author = name2)
 
 
+@app.route('/results', methods=["POST"])
+def results():
+   color = request.form['color']
+   luck_num = request.form['luck_num']
+   fav_class = request.form['fav_class']
+   best_pix = request.form['best_pix'].lower().strip()
+   films = ["toy story","a bug's life","toy story 2","monsters, inc.",
+      "finding nemo", "the incredibles","cars","ratatouille","wall-e","up",
+      "toy story 3","cars 2", "brave","monsters university","inside out",
+      "the good dinosaur","finding dory", "cars 3","coco","incredibles 2",
+      "toy story 4","onward","soul"]
+   colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+   if best_pix not in films:
+      best_pix = "Sorry, '{0}' isn't a Pixar film.".format(best_pix.title())
+   else:
+      best_pix = best_pix.title()
 
+   if color not in colors:
+      color = "Sorry, '{0}' isn't a color option.".format(color.title())
+   else:
+      color = color.title()
+
+   return render_template('form_results.html', color = color, luck_number = luck_num, fav_class = fav_class, best_pix = best_pix)
 
 
 if __name__ == '__main__':
